edfb6647582874e757ef39b9a6ff7440b0ede826
Oleksandr Mohylivskyi <mogilevskiyalexandrtm@gmail.com>
2019-04-22 14:40:07 +0300

    add accordion

--- project/common.js
+++ project/common.js
@@ -1,13 +1,44 @@
 function ready() {
+    let accordion = document.getElementsByClassName('card-header');
+    let elem = document.getElementById('menu-icon');
+
+    function toggleMenu() {
+        let menu = document.getElementById('menu');
+        menu.classList.toggle('open');
+    }
+
+    function toggleAccordion(arr) {
+        let elem = arr;
+        for(let i=0; i < elem.length; i++) {
+            elem[i].addEventListener('click', function() {
+                this.classList.toggle('active');
+                let body = this.nextElementSibling;
+                if(body.style.maxHeight) {
+                    body.style.maxHeight = null;
+                } else {
+                    body.style.maxHeight = body.scrollHeight + 'px';
+                }
+            })
+        }
+    }
+
     function toggleFooter(id) {
         let pageYOffset = window.pageYOffset;
         let elem = document.getElementById(id);
-        let scrolled = !!pageYOffset; 
+        let scrolled = !!pageYOffset;
         scrolled ? elem.classList.remove("hide") : elem.classList.add('hide');
     }
+
+    toggleAccordion(accordion);
+
     window.onscroll = function() {
         toggleFooter('footer')
-    }
+    };
+
+    elem.addEventListener('click', toggleMenu);
+    console.log(elem)
+
+
 }
 document.addEventListener('DOMContentLoaded', ready);
 
