2a2d1871835a78299d6f42328ad85407215a0126
vteplytskyi <teplytskyy.vitaliyvinnytsya@gmail.com>
2019-04-23 18:21:45 +0300

    fixing header

--- project/common.js
+++ project/common.js
@@ -1,26 +1,54 @@
 function ready() {
-    let accordion = document.getElementsByClassName('card-header');
-    let elem = document.getElementById('menu-icon');
+    let accordion = document.querySelector('.accordion');
+    let mobIcon = document.getElementById('menu-icon');
+    let menu = document.getElementById('menu');
 
     function toggleMenu() {
-        let menu = document.getElementById('menu');
         menu.classList.toggle('open');
     }
 
-    function toggleAccordion(arr) {
-        let elem = arr;
-        for(let i=0; i < elem.length; i++) {
-            elem[i].addEventListener('click', function() {
-                this.classList.toggle('active');
-                let body = this.nextElementSibling;
-                if(body.style.maxHeight) {
-                    body.style.maxHeight = null;
-                } else {
-                    body.style.maxHeight = body.scrollHeight + 'px';
+    function scrollIt(element) {
+        // let additionalOffset = 0;
+        // if(menu.classList.contains('open')) {
+        //     additionalOffset = 112;
+        // }
+        window.scrollTo({
+            'behavior': 'smooth',
+            'left': 0,
+            'top': element.offsetTop - 50
+        });
+    }
+
+    const btns = document.querySelectorAll('.js-btn');
+    const sections = document.querySelectorAll('.js-section');
+
+    btns[0].addEventListener('click', () => scrollIt(sections[0]));
+    btns[1].addEventListener('click', () => scrollIt(sections[1]));
+    btns[2].addEventListener('click', () => scrollIt(sections[2]));
+
+
+    function toggleAccordion(el) {
+        el.onclick = function(event) {
+            let target = event.target;
+            while (!target.classList.contains('accordion')) {
+                if(target.classList.contains('card')) {
+                    if(target.classList.contains('active')) {
+                        target.classList.remove('active');
+                        return
+                    }
+                    let elem = this.children;
+                    for(let i = 0; i < elem.length; i++) {
+                        elem[i].classList.remove('active');
+                    }
+                    target.classList.add('active');
+                    return;
                 }
-            })
+                target = target.parentNode;
+            }        
         }
     }
+
+    toggleAccordion(accordion);
 
     function toggleFooter(id) {
         let pageYOffset = window.pageYOffset;
@@ -29,15 +57,31 @@ function ready() {
         scrolled ? elem.classList.remove("hide") : elem.classList.add('hide');
     }
 
-    toggleAccordion(accordion);
-
     window.onscroll = function() {
-        toggleFooter('footer')
+        toggleFooter('footer');
+        //close the menu when scrolling
+        menu.classList.remove('open');
     };
 
-    elem.addEventListener('click', toggleMenu);
+    mobIcon.addEventListener('click', toggleMenu);
 
+    (function() {
+        window.addEventListener('resize', resizeThrottler, false);
+        let resizeTimeout;
+        function resizeThrottler() {
+            if(!resizeTimeout) {
+                resizeTimeout = setTimeout(function() {
+                    resizeTimeout = null;
+                    actualResizeHandkler();
+                }, 60)
+            }
+        }
+        function actualResizeHandkler() {
+            menu.classList.remove('open');
+        };
+    }())
 
 }
+
 document.addEventListener('DOMContentLoaded', ready);
 
